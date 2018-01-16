a93b76519aedb91dae1dc12bcab144a0647ae34a
jRimbault <jacques.rimbault@gmail.com>
2018-01-16 08:46:23 +0100

    tiles the pictures in a masonry-like pattern
    Former-commit-id: e702aba4e0d05148a749167acc8452c013971685

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,19 +11,30 @@ function removeExtension(input) {
     return input.replace(/\.[^/.]+$/, "");
 }
 
-function buildImgCard(id, name) {
-    let title = removeExtension(name);
-    let card = $('<div>');
-    card.attr('class', 'card');
+function buildCardImg(id, name) {
+    let img = $('<img>');
+    img.attr('class', 'card-img-top');
+    img.attr('src', 'assets/img/' + id + '/thumbnails/' + name);
+    img.attr('title', toTitleCase(removeExtension(name)));
+    return img;
+}
+
+function buildImgLink(id, name) {
     let link = $('<a>');
     link.attr('href', 'assets/img/' + id + '/' + name);
-    link.attr('id', title);
-    let img = $('<img>');
-    img.attr('src', 'assets/img/' + id + '/thumbnails/' + name);
-    img.attr('title', toTitleCase(title));
-    img.css('max-width', '100%');
+    link.attr('id', toTitleCase(removeExtension(name)));
+    return link;
+}
+
+function buildImgCard(id, name) {
+    let card = $('<div>');
+    card.attr('class', 'card');
+
+    let link = buildImgLink(id, name);
+    let img = buildCardImg(id, name);
     img.appendTo(link);
     link.appendTo(card);
+
     return card;
 }
 
