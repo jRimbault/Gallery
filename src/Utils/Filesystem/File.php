3b54d9dcc0fdf7d4601a7b9eeda49c1f616587cf
jRimbault <jacques.rimbault@gmail.com>
2018-01-26 02:51:07 +0100

    post install cmd also deploys an example config

--- src/Utils/Filesystem/File.php
+++ src/Utils/Filesystem/File.php
@@ -38,6 +38,11 @@ class File
         } else {
             echo "  Couldn't install Bootstrap properly" . PHP_EOL;
         }
+        if (self::makeIniConf()) {
+            echo "  Configuration file: `config/app.ini`" . PHP_EOL;
+        } else {
+            echo "  Configuration file already exists (`config/app.ini`)" . PHP_EOL;
+        }
     }
 
     /** Mostly stolen from a comment on the php manual */
@@ -56,5 +61,68 @@ class File
             return true;
         }
         return false;
+    }
+
+    private static function makeIniConf()
+    {
+        $confFile = Path::Root() . '/config/app.ini';
+        if (file_exists($confFile)) return false;
+        $conf = [
+            'SITE' => [
+                'title' => 'Example',
+                'about' => 'Some text about the website',
+                'email' => 'email@example.org'
+            ],
+            'COLOR' => [
+                'background' => '212529',
+                'lightbox' => '212529'
+            ],
+            'LINK' => [
+                'url' => [
+                    'https://www.example.org',
+                    'https://www.google.com',
+                    'https://you.can-set.an-url.without/a-text'
+                ],
+                'text' => [
+                    'A website example',
+                    'Google'
+                ]
+            ],
+            'SWITCH' => [
+                'theater' => false
+            ]
+        ];
+        return self::put_ini_file($confFile, $conf);
+    }
+
+    /** This is tailor made and not really good */
+    private static function put_ini_file($file, $array, $i = false)
+    {
+        $str = "";
+        foreach ($array as $k => $v) {
+            if (is_array($v)) {
+                if ($i === false) {
+                    $str .= "[$k]" . PHP_EOL;
+                } else {
+                    foreach ($v as $v2) {
+                        $str .= $k . "[] = \"$v2\"" . PHP_EOL;
+                    }
+                }
+                $str .= self::put_ini_file("", $v, true);
+            } else {
+                if (!is_numeric($k) && !is_bool($v)) {
+                    $str .= "$k = \"$v\"" . PHP_EOL;
+                }
+                if (is_bool($v)) {
+                    $sub = $v ? 'true' : 'false';
+                    $str .= "$k = " . $sub . PHP_EOL;
+                }
+            }
+        }
+        if ($file) {
+            return file_put_contents($file, $str);
+        } else {
+            return $str;
+        }
     }
 }
