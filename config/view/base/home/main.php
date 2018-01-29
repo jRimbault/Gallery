9140ee65dc6431b35d4bc327b930d476cb1a6ca4
jRimbault <jacques.rimbault@gmail.com>
2018-01-29 19:52:02 +0100

    implements gallery card building server side

--- config/view/base/home/main.php
+++ config/view/base/home/main.php
@@ -1,13 +1,36 @@
+<?php
+
+use Gallery\Path;
+use Gallery\Utils\Filesystem\Scan;
+
+function buildGalleryCards()
+{
+    $html = "";
+    $scanner = new Scan(Path::Gallery());
+    foreach ($scanner->getGalleries() as $gallery) {
+        $html .=
+            "<a href='unsplash'>
+                <div class='card text-white bg-dark'>
+                    <img class='card-img-top rounded' src='gallery/$gallery.jpg'>
+                    <div class='card-img-overlay'>
+                        <h4 class='card-title'>".ucfirst($gallery)."</h4>
+                    </div>
+                </div>
+            </a>";
+    }
+    return $html;
+}
+
+?>
 <main role="main">
-    <!--<section class="jumbotron text-center" id="lead">
-        <div class="container">
-            <h1 class="jumbotron-heading">Chez Rimbault</h1>
-        </div>
-    </section>-->
     <div class="album text-muted">
         <div class="container">
             <div class="card-columns" id="gallery">
-
+<?php
+if (!$conf->getSinglePage()) {
+    echo buildGalleryCards();
+}
+?>
             </div>
         </div>
     </div>
