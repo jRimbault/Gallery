2ff6ab2955a44fbffe62ba6001973ce83456b55a
jRimbault <jacques.rimbault@gmail.com>
2018-02-02 17:14:55 +0100

    add comments and path contructor
    instead of quick dirty hard coded paths

--- src/Path.php
+++ src/Path.php
@@ -2,13 +2,36 @@
 
 namespace Gallery;
 
-use Gallery\Utils\Config;
-
 
 class Path
 {
     public static function Root() { return dirname(__DIR__); }
-    public static function Gallery() { return self::Root() . '/public/gallery'; }
-    public static function View() { return self::Root() . '/config/view'; }
-    public static function AssetsLib() { return self::Root() . '/public/assets/lib'; }
+
+    /** Shortcut to the gallery directory */
+    public static function Gallery() {
+        return join(DIRECTORY_SEPARATOR, [
+            self::Root(),
+            'public',
+            'gallery',
+        ]);
+    }
+
+    /** Shortcut to the base view directory */
+    public static function View() {
+        return join(DIRECTORY_SEPARATOR, [
+            self::Root(),
+            'config',
+            'view',
+        ]);
+    }
+
+    /** Shortcut to the front-end libraries directory */
+    public static function AssetsLib() {
+        return join(DIRECTORY_SEPARATOR, [
+            self::Root(),
+            'public',
+            'assets',
+            'lib',
+        ]);
+    }
 }
