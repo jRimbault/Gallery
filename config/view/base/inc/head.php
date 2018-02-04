d47125eca28dad450943301352b7718b2858b9a7
jRimbault <jacques.rimbault@gmail.com>
2018-02-04 01:38:48 +0100

    feat: adds html meta description
    Description got from the about section

--- config/view/base/inc/head.php
+++ config/view/base/inc/head.php
@@ -3,6 +3,7 @@
     <meta name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?php echo $conf->getTitle(); ?></title>
+    <meta name="description" content="<?php echo $conf->getAbout(); ?>">
     <link rel="stylesheet"
           href="/assets/lib/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet"
