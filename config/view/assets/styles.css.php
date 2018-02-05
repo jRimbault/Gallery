c1d4fa5f4144bb0a6fdcb0b11e25d2bcd52b1d02
jRimbault <jacques.rimbault@gmail.com>
2018-02-05 18:28:42 +0100

    feat(Config): add class Color
    allow various computations about colors
    in particular getting the lightness of a color
    
    fix(Error):
      the 404 error page text stayed white even on light
      backgrounds

--- config/view/assets/styles.css.php
+++ config/view/assets/styles.css.php
@@ -2,28 +2,40 @@
 
 header('Content-Type: text/css');
 
-use Gallery\Utils\Config;
 use Gallery\Path;
+use Gallery\Utils\Color;
+use Gallery\Utils\Config;
 
 $conf = Config::Instance();
 
-function isHexColor($input)
-{
-    if (!ctype_xdigit($input)) return false;
-    if (!in_array(strlen($input), [3, 6])) return false;
-    return true;
+$bg = false;
+$lb = false;
+
+if ($conf->getBackground()) {
+    try {
+        $bg = new Color($conf->getBackground());
+    } catch (\Exception $e) {
+        $bg = false;
+    }
+}
+if ($conf->getLightbox()) {
+    try {
+        $lb = new Color($conf->getLightbox());
+    } catch (\Exception $e) {
+        $lb = false;
+    }
 }
 
 require_once Path::View() . '/assets/static/styles.css';
 
-if (isHexColor($conf->getBackground())) {
+if ($bg) {
 ?>
 .bg-dark {
-    background-color: #<?php echo $conf->getBackground(); ?> !important;
+    background-color: <?php echo $bg; ?> !important;
 }
 <?php
-$hsl = RGBtoHSL(HTMLtoRGB($conf->getBackground()));
-if ($hsl->lightness > 200) {
+
+if ($bg->getLightness() > 200) {
 ?>
 ul li a.text-white, .navbar .navbar-brand {
     color: #000000 !important;
@@ -35,74 +47,10 @@ ul li a.text-white, .navbar .navbar-brand {
 }
 
 }
-if (isHexColor($conf->getLightbox())) {
+if ($lb) {
 ?>
 .ekko-lightbox .modal-content {
-    background-color: #<?php echo $conf->getLightbox(); ?> !important;
+    background-color: <?php echo $lb; ?> !important;
 }
 <?php
-}
-
-function HTMLtoRGB($htmlCode)
-{
-    $htmlCode = ltrim($htmlCode, '#');
-    if (strlen($htmlCode) === 3) {
-        $htmlCode = $htmlCode[0] . $htmlCode[0]
-                  . $htmlCode[1] . $htmlCode[1]
-                  . $htmlCode[2] . $htmlCode[2];
-    }
-    $r = hexdec($htmlCode[0] . $htmlCode[1]);
-    $g = hexdec($htmlCode[2] . $htmlCode[3]);
-    $b = hexdec($htmlCode[4] . $htmlCode[5]);
-
-    return $b + ($g << 0x8) + ($r << 0x10);
-}
-
-function RGBtoHSL($RGB)
-{
-    $r = 0xFF & ($RGB >> 0x10);
-    $g = 0xFF & ($RGB >> 0x8);
-    $b = 0xFF & $RGB;
-
-    $r = ( (float) $r ) / 255.0;
-    $g = ( (float) $g ) / 255.0;
-    $b = ( (float) $b ) / 255.0;
-
-    $maxColor = max([$r, $g, $b]);
-    $minColor = min([$r, $g, $b]);
-
-    $lightness = ($maxColor + $minColor) / 2.0;
-
-    if ($maxColor == $minColor) {
-        $saturation = 0;
-        $hue = 0;
-    } else {
-        if ($lightness < .5) {
-            $saturation = ($maxColor - $minColor) / ($maxColor + $minColor);
-        } else {
-            $saturation = ($maxColor - $minColor) / (2.0 - $maxColor + $minColor);
-        }
-        switch ($maxColor) {
-            case $r:
-                $hue = ($g - $b) / ($maxColor - $minColor);
-                break;
-            case $g;
-                $hue = 2.0 + ($b - $r) / ($maxColor - $minColor);
-                break;
-            case $b;
-                $hue = 4.0 + ($r - $g) / ($maxColor - $minColor);
-                break;
-        }
-        $hue /= 6.0;
-    }
-
-    $hue = (int) round(255.0 * $hue);
-    $saturation = (int) round(255.0 * $saturation);
-    $lightness = (int) round(255.0 * $lightness);
-
-    return (object) [
-        'hue' => $hue,
-        'saturation' => $saturation,
-        'lightness' => $lightness
-    ];
 }
---
+++ src/Utils/Color.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Gallery\Utils;
+
+
+class Color
+{
+    private $color;
+    private $rgb;
+
+    private $red;
+    private $green;
+    private $blue;
+
+    private $hue;
+    private $saturation;
+    private $lightness;
+
+    public function __construct(string $string)
+    {
+        $string = ltrim($string, '#');
+        if (!$this->isHexColor($string)) {
+            throw new \Exception('String isn\'t an rgb or rrggbb color');
+        }
+        $this->color = $string;
+        $this->toHex();
+        $this->toHSL();
+    }
+
+    /**
+     * Sanity check before proceeding
+     * Could be improved by accepting other format than RRGGBB
+     * and supply to the RRGGBB string needed by the other functions
+     */
+    private function isHexColor(string $string)
+    {
+        if (!ctype_xdigit($string)) return false;
+        if (!in_array(strlen($string), [3, 6])) return false;
+        return true;
+    }
+
+    /** Mainly here because it allows the computation of the HSL values */
+    private function toRGB()
+    {
+        if (strlen($this->color) === 3) {
+            $this->color = $this->color[0] . $this->color[0]
+                    . $this->color[1] . $this->color[1]
+                    . $this->color[2] . $this->color[2];
+        }
+        $this->red = hexdec($this->color[0] . $this->color[1]);
+        $this->green = hexdec($this->color[2] . $this->color[3]);
+        $this->blue = hexdec($this->color[4] . $this->color[5]);
+
+        return $this->rgb = $this->blue + ($this->green << 0x8) + ($this->red << 0x10);
+    }
+
+    /**
+     * Computes the HSL (Hue, Saturation, Lightness) of the color
+     * I want this to determine if the color is light or dark
+     * to change the color of the text accordingly
+     */
+    private function toHSL()
+    {
+        $rgb = $this->toRGB();
+        $r = 0xFF & ($rgb >> 0x10);
+        $g = 0xFF & ($rgb >> 0x8);
+        $b = 0xFF & $rgb;
+
+        $r = ( (float) $r ) / 255.0;
+        $g = ( (float) $g ) / 255.0;
+        $b = ( (float) $b ) / 255.0;
+
+        $maxColor = max([$r, $g, $b]);
+        $minColor = min([$r, $g, $b]);
+
+        $lightness = ($maxColor + $minColor) / 2.0;
+
+        if ($maxColor == $minColor) {
+            $saturation = 0;
+            $hue = 0;
+        } else {
+            if ($lightness < .5) {
+                $saturation = ($maxColor - $minColor) / ($maxColor + $minColor);
+            } else {
+                $saturation = ($maxColor - $minColor) / (2.0 - $maxColor + $minColor);
+            }
+            switch ($maxColor) {
+                case $r:
+                    $hue = ($g - $b) / ($maxColor - $minColor);
+                    break;
+                case $g;
+                    $hue = 2.0 + ($b - $r) / ($maxColor - $minColor);
+                    break;
+                case $b;
+                    $hue = 4.0 + ($r - $g) / ($maxColor - $minColor);
+                    break;
+            }
+            $hue /= 6.0;
+        }
+
+        $this->hue = (int) round(255.0 * $hue);
+        $this->saturation = (int) round(255.0 * $saturation);
+        $this->lightness = (int) round(255.0 * $lightness);
+    }
+
+    /** Magic getter method */
+    public function __call($method, $params = [])
+    {
+        $var = strtolower(substr($method, 3));
+        if (strncasecmp($method, 'get', 3) === 0 && isset($this->$var)) {
+            return $this->$var;
+        }
+        return null;
+    }
+
+    public function __toString()
+    {
+        return '#' . $this->color;
+    }
+}
