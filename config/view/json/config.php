249d42e302ea5ea93e2a4c27e1ef55d136d26821
jRimbault <jacques.rimbault@gmail.com>
2018-02-06 10:46:12 +0100

    fix(Request): change attributes visibility

--- config/view/json/config.php
+++ config/view/json/config.php
@@ -14,19 +14,19 @@ $request = new Request();
 $conf = [
     'site' => [],
     'color' => [],
-    'link' =>  $request->getPost('link') ?? [],
+    'link' =>  $request->post->get('link') ?? [],
     'switch' => [],
 ];
 
-$conf['site']['title'] = $request->getPost('title') ?? '';
-$conf['site']['about'] = $request->getPost('about') ?? '';
-$conf['site']['email'] = $request->getPost('email') ?? '';
+$conf['site']['title'] = $request->post->get('title') ?? '';
+$conf['site']['about'] = $request->post->get('about') ?? '';
+$conf['site']['email'] = $request->post->get('email') ?? '';
 
-$conf['color']['background'] = removeHash($request->getPost('background') ?? '');
-$conf['color']['lightbox'] = removeHash($request->getPost('lightbox') ?? '');
+$conf['color']['background'] = removeHash($request->post->get('background') ?? '');
+$conf['color']['lightbox'] = removeHash($request->post->get('lightbox') ?? '');
 
-$conf['switch']['dev'] = ($request->getPost('dev') == 'true');
-$conf['switch']['singlepage'] = ($request->getPost('singlepage') == 'true');
+$conf['switch']['dev'] = ($request->post->get('dev') == 'true');
+$conf['switch']['singlepage'] = ($request->post->get('singlepage') == 'true');
 $conf['switch']['theater'] = true;
 
 $file = Path::Root() . '/config/app.json';
--- src/Utils/Http/Request.php
+++ src/Utils/Http/Request.php
@@ -12,30 +12,20 @@ use Gallery\Utils\Http\Server;
  */
 class Request
 {
-    private $getData;
-    private $post;
-    private $server;
+    public $get;
+    public $post;
+    public $server;
 
     public function __construct()
     {
         $this->post = new Post();
         $this->server = new Server();
-        $this->getData = new Get();
+        $this->get = new Get();
     }
 
     public function getRequest(string $index)
     {
         return $this->server->get('REQUEST_' . strtoupper($index));
-    }
-
-    public function getPost(string $index)
-    {
-        return $this->post->get($index);
-    }
-
-    public function getGet(string $index)
-    {
-        return $this->getData->get($index);
     }
 
     /**
