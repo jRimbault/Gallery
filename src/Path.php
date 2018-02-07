d7b2896ecded74139bbc376e97ed613573bed6bb
jRimbault <jacques.rimbault@gmail.com>
2018-02-08 00:04:51 +0100

    feat(Path): add methods
    - 'name': get the filename or directory name targeted
    - 'extension': get extension, if any, of the target
    - 'atomize': split path into its parts

--- src/Path.php
+++ src/Path.php
@@ -12,9 +12,45 @@ class Path
         $this->path = str_replace('/', DIRECTORY_SEPARATOR, $path);
     }
 
-    public function __toString() { return self::Root() . $this->path; }
+    public function __toString()
+    {
+        if ($this->path[0] === DIRECTORY_SEPARATOR) {
+            return self::Root() . $this->path;
+        }
+        return $this->path;
+    }
 
-    public static function Root() { return dirname(__DIR__); }
+    public function name()
+    {
+        return basename($this->path);
+    }
+
+    public function extension()
+    {
+        if (strpos($this->name(), '.') === false) {
+            return $this->name();
+        }
+        $array = explode('.', $this->name());
+        return end($array);
+    }
+
+    public function atomize()
+    {
+        return array_filter(explode(
+            DIRECTORY_SEPARATOR,
+            $this->path
+        ));
+    }
+
+    /**
+     * Collection of static methods
+     */
+
+    /** Short for the root directory of the current project */
+    public static function Root()
+    {
+        return dirname(__DIR__);
+    }
 
     /** Shortcut to the gallery directory */
     public static function Gallery() {
