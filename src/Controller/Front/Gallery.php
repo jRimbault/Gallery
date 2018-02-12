e89fab3171f6bb89d39824f6da45aa3afe2d1d74
jRimbault <jacques.rimbault@gmail.com>
2018-02-12 12:54:34 +0100

    imp(Twig): replace some views by Twig templates

--- src/Controller/Front/Gallery.php
+++ src/Controller/Front/Gallery.php
@@ -4,15 +4,23 @@ namespace Gallery\Controller\Front;
 
 use Gallery\Path;
 use Gallery\Utils\Json;
+use Gallery\Utils\Config;
 use Gallery\Utils\Http\Request;
 use Gallery\Utils\Filesystem\Scan;
+use Gallery\Controller\Controller;
 
 
-class Gallery
+class Gallery extends Controller
 {
-    public static function page()
+    public static function page(Request $request)
     {
-        require new Path('/config/view/base/gallery.php');
+        $scanner = new Scan(Path::Gallery());
+        $gallery = $request->server()->getRequest('uri');
+        self::render('gallery.html.twig', [
+            'conf'        => Config::Instance(),
+            'galleryName' => $gallery,
+            'gallery'     => $scanner->getGallery($gallery),
+        ]);
     }
 
     public static function galleries()
